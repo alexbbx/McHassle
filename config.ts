49e26e4c4ceeda5126dd7c6097f4f414c271d102
alexbbx <109413241+alexbbx@users.noreply.github.com>
2024-08-23 21:09:22 +0300

    edit

--- config.ts
+++ config.ts
@@ -11,8 +11,8 @@ export const window = {
 };
 
 export const api = {
-    ws: 'ws://127.0.0.1:1370/ws',
-    web: 'http://127.0.0.1:1370',
+    ws: 'ws://203.31.40.166:1370//ws',
+    web: 'http://203.31.40.166:1370/',
     // extraToken: token,
 };
 
